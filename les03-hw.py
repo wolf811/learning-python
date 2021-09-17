f08ed0002d31414d97e94a7ecb0816219427bded
wolf811 <wolf811@gmail.com>
2021-09-18 00:23:50 +0300

    end task1,2,3

--- les03-hw.py
+++ les03-hw.py
@@ -14,9 +14,20 @@
 
 # ============= Задание 2 ===========
 
-def user (**k)
+# def user (**kwargs):
+#     res = list(kwargs.values())
+#     return ', '.join(res)
+
+# print(user(name = 'Валентин', surname = 'Комбаров', year = '1981', city = 'Домодедово', email = 'wolf811@gmail.com', phone = '89773570448' ))
 
 # ============= Задание 3 ===========
+
+def my_func(a,b,c):
+    my_list = [a,b,c]
+    n_min = my_list.remove(min(my_list))
+    return sum(my_list)
+print(my_func(10,5,6))
+
 # ============= Задание 4 ===========
 # ============= Задание 5 ===========
 # ============= Задание 6 ===========
