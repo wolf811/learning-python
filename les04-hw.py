695f7c0ba1978d4a69fb483fd0053a7f7e8bcdfa
wolf811 <wolf811@gmail.com>
2021-09-21 17:00:27 +0300

    tasl 1-4 end

---
+++ les04-hw.py
@@ -0,0 +1,30 @@
+from les04_payroll import payroll
+from functools import reduce
+# ============= Задание 1 ===========
+# print(payroll())
+
+# ============= Задание 2 ===========
+
+# first_list = [2, 52, 86, 123, 100]
+# second_list = [el+1 for el in first_list]
+# print(f"Первый список: {first_list}")
+# print(f"Второй список: {second_list}")
+
+# ============= Задание 3 ===========
+# print([el/20 or el/21 for el in range(20, 240)])
+
+# ============= Задание 4 ===========
+# list_a = [2, 2, 2, 7, 23, 1, 44, 44, 3, 2, 10, 7, 4, 11]
+# list_b = [i for i in list_a if list_a.count(i) == 1]
+# # list_b = []
+# # for i in list_a:
+# #     if list_a.count(i) == 1:
+# #         list_b.append(i)
+# print(list_b)
+
+# ============= Задание 5 ===========
+list_c = [i for i in range(100, 200, 2)]
+print(list_c)
+
+# ============= Задание 6 ===========
+# ============= Задание 7 ===========
