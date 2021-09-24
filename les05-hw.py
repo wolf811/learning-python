e6c682eeb5c84d1aedb61b8d62fedc0f7ce3718c
wolf811 <wolf811@gmail.com>
2021-09-24 16:13:52 +0300

    task 2 in work

--- les05-hw.py
+++ les05-hw.py
@@ -1,10 +1,19 @@
 # ============= Задание 1 ===========
-lst = ['stroka_1\n', 'stroka_2\n', 'stroka_3\n']
-with open(r"les05-hw_for_file/f_task1.txt", 'w') as f_task1:
-    f_task1.writelines(lst)
+# lst = []
+# with open(r"les05-hw_for_file/f_task1.txt", 'w') as f_task1:
+#     while True:
+#         text = input("Введите текст: ")
+#         lst.append(text + "\n")
+#         if text == "":
+#             break
 
+#     f_task1.writelines(lst)
+# print(lst)
 
 # ============= Задание 2 ===========
+with open(r"les05-hw_for_file/f_task2.txt", encoding="utf-8") as f_task2:
+    for line in f_task2:
+        print(line)
 # ============= Задание 3 ===========
 # ============= Задание 4 ===========
 # ============= Задание 5 ===========
