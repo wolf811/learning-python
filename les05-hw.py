c050a7ab3a25fd7a00d43ddb0e814ba701e2ceaa
wolf811 <wolf811@gmail.com>
2021-09-25 21:18:08 +0300

    task3 in work

--- les05-hw.py
+++ les05-hw.py
@@ -22,11 +22,25 @@
 
 # ============= Задание 3 ===========
 with open(r"les05-hw_for_file/f_task3.txt", encoding="utf-8") as f_task3:
+    
     lines = f_task3.readlines()
-    my_dict = {}
-    for i in lines:
-        my_dict.update([i])
-        print(my_dict)
+    average = sum
+    for line in lines:
+        lst_line = line.split()
+        lst_line[1] = int(lst_line[1])
+        # for i in range(len(lst_line)):
+        #     try:
+        #         lst_line[i] = int(lst_line[i])
+        #     except ValueError:
+        #         pass
+
+        if lst_line[1] < 20000:
+            print(f"Сотрудник с зарплатой менее 20 000 руб.: \n {line}")
+        # average = lst_line[1] * 5 / 
+        # print(average)
+
+
 # ============= Задание 4 ===========
 # ============= Задание 5 ===========
+
 # ============= Задание 6 ===========
