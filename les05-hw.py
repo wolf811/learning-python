f0d91928d94783fd25646984388743c56a7b5b90
wolf811 <wolf811@gmail.com>
2021-09-25 11:21:51 +0300

    task3 in work

--- les05-hw.py
+++ les05-hw.py
@@ -11,10 +11,22 @@
 # print(lst)
 
 # ============= Задание 2 ===========
-with open(r"les05-hw_for_file/f_task2.txt", encoding="utf-8") as f_task2:
-    for line in f_task2:
-        print(line)
+# with open(r"les05-hw_for_file/f_task2.txt", encoding="utf-8") as f_task2:
+#     lines = f_task2.readlines()
+#     print("Количество строк в файле: ", len(lines))
+#     i = 0
+#     for line in lines:
+#         i += 1
+#         x = len(line.split())
+#         print(f"Количество слов в строке {i}: {x}")
+
 # ============= Задание 3 ===========
+with open(r"les05-hw_for_file/f_task3.txt", encoding="utf-8") as f_task3:
+    lines = f_task3.readlines()
+    my_dict = {}
+    for i in lines:
+        my_dict.update([i])
+        print(my_dict)
 # ============= Задание 4 ===========
 # ============= Задание 5 ===========
 # ============= Задание 6 ===========
