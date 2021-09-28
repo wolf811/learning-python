32238b19ac51ce764e06f51a8847ab4ba9b1cc75
wolf811 <wolf811@gmail.com>
2021-09-28 16:55:29 +0300

    task1 les06

---
+++ les06-hw.py
@@ -0,0 +1,12 @@
+# ============= Задание 1 ===========
+class TrafficLight:
+    __color = None
+    def __running(self, time):
+        self.time = time
+        TrafficLight.__color = color
+
+
+a = TrafficLight()
+print(f'Запускаем светофор')
+a.running(a__color, 7)
+
