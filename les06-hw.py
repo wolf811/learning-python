535cfef6bdeff9617a770200811f9c6d28bc2788
wolf811 <wolf811@gmail.com>
2021-09-29 08:48:04 +0300

    fix les06

--- les06-hw.py
+++ les06-hw.py
@@ -1,12 +1,24 @@
 # ============= Задание 1 ===========
 class TrafficLight:
-    __color = None
-    def __running(self, time):
-        self.time = time
-        TrafficLight.__color = color
+    __color = ["Red", "Yellow", "Green"]
 
+    def running(self, time_green, time_red=7, time_yellow=2):
+        print("Светофор запустили:")
+        for i in TrafficLight.__color:
+            if i == TrafficLight.__color[0]:
+                print(f"{i} - {time_red} sec")
+            elif i == TrafficLight.__color[1]:
+                print(f"{i} - {time_yellow} sec")
+            else:
+                print(f"{i} - {time_green} sec")
 
-a = TrafficLight()
-print(f'Запускаем светофор')
-a.running(a__color, 7)
+run = TrafficLight()
+run.running(8)
 
+# ============= Задание 2 ============
+class Road:
+    _lenght = 0
+    _width = 0
+
+    def mass_asphalt(self, mass, depth):
+        pass
