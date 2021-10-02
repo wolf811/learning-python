6eeb5d8a5461b9070b81942b3e4aad459e559f1c
wolf811 <wolf811@gmail.com>
2021-10-02 22:19:09 +0300

    task1

---
+++ les07-hw.py
@@ -0,0 +1,16 @@
+# ============= Задание 1 ===========
+# class Matrix:
+#     def __init__(self):
+#         self.mylist = [[],[],[]]
+#         print(self.mylist)
+
+# mtr = Matrix()
+
+# ============= Задание 2 ===========
+mylist = []
+row = 3
+col = 3
+for i in range(row):
+    mylist.append([1] * col)
+
+print(mylist)
