a43d3693f000ed6c52bfd4ca05900fc2d0d25d9d
wolf811 <wolf811@gmail.com>
2021-10-03 15:28:03 +0300

    fix

--- les07-hw.py
+++ les07-hw.py
@@ -1,16 +1,26 @@
+from random import randrange
 # ============= Задание 1 ===========
-# class Matrix:
-#     def __init__(self):
-#         self.mylist = [[],[],[]]
-#         print(self.mylist)
+class Matrix:
+    def __init__(self, row, col):
+        self.row = row
+        self.col = col
+        self.mtr = [[randrange(0,10) for y in range(col)] for x in range(row)]
+        for el in range(col):
+            print(self.mtr[el])
+        print("===============")
 
-# mtr = Matrix()
+    def __str__(self):
+        for self.row in self.mtr:
+            return i
+            
+
+    def __add__(self):
+        pass
+
+matrix = Matrix(3,3)
+# matrix_str = Matrix(3, 3)
+print(matrix)
+
 
 # ============= Задание 2 ===========
-mylist = []
-row = 3
-col = 3
-for i in range(row):
-    mylist.append([1] * col)
 
-print(mylist)
