68b45bf92edf1994438a1b3660b7162318faa9b9
wolf811 <wolf811@gmail.com>
2022-03-11 11:46:06 +0300

    end task1-2

--- algoritms/les01/5.py
+++ algoritms/les01/5.py
@@ -1,16 +1,20 @@
 # 5. Пользователь вводит две буквы. Определить, на каких местах алфавита они стоят и сколько между ними находится букв.
 
 def get_alf():
-    let1 = input('Enter first letter: ')
-    # let2 = input('Enter second letter: ')
-    alf = {
-        '1': 'a',
-        '2': 'b',
-        '3': 'c',
-    }
-    for k,v in alf.items():
-        if v == let1:
-            return k
+    let1 = input('Введите первую букву: ')
+    let2 = input('Введите вторую букву: ')
+    pos_let1 = ord(let1) - 64
+    pos_let2 = ord(let2) - 64
+    # return f'Места букв в алфавите: {let1}, {let2}'
+    return f'{let1} - на месте {ord(let1)}; {let2} - на месте {ord(let2)}'
+    # alf = {
+    #     '1': 'a',
+    #     '2': 'b',
+    #     '3': 'c',
+    # }
+    # for k,v in alf.items():
+    #     if v == let1:
+    #         return k
 
 
 print(get_alf())
