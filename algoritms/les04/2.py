daff894db84db378b353af63be6a0b83a8c61703
wolf811 <wolf811@gmail.com>
2022-03-31 09:06:29 +0300

    les04 end

--- algoritms/les04/2.py
+++ algoritms/les04/2.py
@@ -6,22 +6,21 @@
 import math
 import timeit
 
-def sieve_without_eratosthenes(i):
+def not_sieve_eratosthenes(i):
     '''Функция поиска i-го простого числа,
     без использования алгоритма «Решето Эратосфена»
     '''
-
     lst_prime = [2]
-    number = 3
+    num = 3
     while len(lst_prime) < i:
         flag = True
         for j in lst_prime.copy():
-            if number % j == 0:
+            if num % j == 0:
                 flag = False
                 break
         if flag:
-            lst_prime.append(number)
-        number += 1
+            lst_prime.append(num)
+        num += 1
     return lst_prime[-1]
 
 
@@ -29,19 +28,17 @@ def sieve_eratosthenes(i):
     '''Функция поиска i-го простого числа,
     используя алгоритм «Решето Эратосфена»
     '''
-
     i_max = prime_counting_function(i)
     lst_prime = [_ for _ in range(2, i_max)]
 
-    for number in lst_prime:
-        if lst_prime.index(number) <= number - 1:
+    for num in lst_prime:
+        if lst_prime.index(num) <= num - 1:
             for j in range(2, len(lst_prime)):
-                if number * j in lst_prime[number:]:
-                    lst_prime.remove(number * j)
+                if num * j in lst_prime[num:]:
+                    lst_prime.remove(num * j)
         else:
             break
     return lst_prime[i - 1]
-
 
 def prime_counting_function(i):
     '''Функция возвращает верхнюю границу отрезка на котором лежат
@@ -50,20 +47,19 @@ def prime_counting_function(i):
     распределения простых чисел. Количество простых чисел на отрезке
     [1;n] растёт с увеличением n как n / ln(n).
     '''
-
-    number_of_primes = 0
-    number = 2
-    while number_of_primes <= i:
-        number_of_primes = number / math.log(number)
-        number += 1
-    return number
+    num_of_primes = 0
+    num = 2
+    while num_of_primes <= i:
+        num_of_primes = num / math.log(num)
+        num += 1
+    return num
 
 
 NUMBER_EXECUTIONS = 1
 TEST_VALUE = 1000
 
-time1 = timeit.timeit(f'sieve_without_eratosthenes({TEST_VALUE})',
-                      setup='from __main__ import sieve_without_eratosthenes',
+time1 = timeit.timeit(f'not_sieve_eratosthenes({TEST_VALUE})',
+                      setup='from __main__ import not_sieve_eratosthenes',
                       number=NUMBER_EXECUTIONS)
 
 time2 = timeit.timeit(f'sieve_eratosthenes({TEST_VALUE})',
@@ -75,5 +71,5 @@ print(f'Программа без использования алгоритма 
 {round(time2 / time1, 2)} раз'
       )
 '''Программа без использования алгоритма «Решето Эратосфена» быстрее
-программы с использованием алгоритма «Решето Эратосфена» в 111.47 раз
+программы с использованием алгоритма «Решето Эратосфена» в 72.28 раз
 '''
